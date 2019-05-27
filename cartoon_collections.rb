963d89449c6f3c600c0d2e397fc7b3ccc67d0c90
Amanda King <amapickell@gmail.com>
2019-05-27 16:29:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves (array)
   array.each_with_index do |name, index|
-    puts (index + 1).to_s +". " + name
+    phrase = (index + 1).to_s +". " + name
+    puts phrase
   end
 end
 
